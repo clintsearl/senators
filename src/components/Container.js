34e93808fa4ed3b5419b49d3d1fe53d56d72b9a5
Clint Earl <clintsearl@gmail.com>
2019-02-15 16:53:10 -0700

    Displaying the name, party, gender, and state.

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,13 +6,17 @@ import './Home.css';
 
 class Container extends Component {
   state = {
-    senator: [],  
+    senators: [],  
   };
+
+  //Or call this a function and then put "componentWillMount after and call the function."
     componentDidMount () {
       fetch('https://raw.githubusercontent.com/CivilServiceUSA/us-senate/master/us-senate/data/us-senate.json')
       .then(senData => {
       return senData.json();
       })
+      // then is returning a promise and thats why we can call one data and the other somehting else
+      // setState is binding it to an object
       .then(data => {
         console.log("data:", data)
         this.setState({senators: data})
@@ -29,6 +33,16 @@ class Container extends Component {
     return (
       
       <div className="Container">
+
+        {this.state.senators.map((senator, index) => 
+        (<div key={index}> <h3>{senator.name}</h3>
+        <p>{senator.party}</p>
+        <p>{senator.gender}</p>
+        <p>{senator.state_name}</p>
+        <p></p>
+
+        </div>)
+        )}
         <p>container</p> 
       </div>
     );
